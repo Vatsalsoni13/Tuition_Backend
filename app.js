5ce1f8adffc403ed0d8cbc3d3f49660090103706
VatsalSoni_13 <sonivatsal2001@gmail.com>
2021-01-07 15:38:37 +0530

    Firts Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+  console.log("HI");
+  res.send("I'm Deployed");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  "Server Started!";
+});
