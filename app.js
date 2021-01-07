978c7a6cd4fce5d7fc81a66fc7f216fe4174603a
VatsalSoni_13 <sonivatsal2001@gmail.com>
2021-01-07 16:35:40 +0530

    Deploy

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./DB/Connection');
-const User = require('./models/User');
-const userRoutes = require('./routes/user');
-const tutorRoutes = require('./routes/tutor');
-const studentRoutes = require('./routes/student');
+const connectDB = require("./DB/Connection");
+const User = require("./models/User");
+const userRoutes = require("./routes/user");
+const tutorRoutes = require("./routes/tutor");
+const studentRoutes = require("./routes/student");
 
 connectDB();
-app.use(express.urlencoded({extended:false}));
-app.use(express.json({extended:false}));
-app.use('/tutor',tutorRoutes);
-app.use('/user',userRoutes);
-app.use('/student',studentRoutes);
-app.use('/',(req,res) =>{
-    console.log("IN");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ extended: false }));
+app.use("/tutor", tutorRoutes);
+app.use("/user", userRoutes);
+app.use("/student", studentRoutes);
+app.use("/", (req, res) => {
+  console.log("IN");
+  res.send("Finally Deployed");
 });
 
-
 const Port = process.env.Port || 3000;
-app.listen(Port,()=>{
-    "started server";
+app.listen(Port, () => {
+  "started server";
 });
