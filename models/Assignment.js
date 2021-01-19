337ee3839159815efd6282179673b9a251a505b9
VatsalSoni_13 <sonivatsal2001@gmail.com>
2021-01-19 18:45:02 +0530

    Assginment preview

--- models/Assignment.js
+++ models/Assignment.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
 const assignment = new mongoose.Schema({
-    path:{
-      type:String
-    },
-    name:{
-    type:String      
-    },
-    batchId: {
-        type: String
-      } ,
-    istDateTime:{
-      type:String
-    },
-    responses:[]
+  path: {
+    type: String,
+  },
+  fileName: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  batchId: {
+    type: String,
+  },
+  istDateTime: {
+    type: String,
+  },
+  responses: [],
 });
 
 module.exports = Assignment = mongoose.model("assignment", assignment);
