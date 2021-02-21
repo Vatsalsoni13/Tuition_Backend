827ac11ed80d422ccf4db1a07dfe62b65b373f26
VatsalSoni_13 <sonivatsal2001@gmail.com>
2021-02-21 14:34:44 +0530

    Updated

--- routes/student.js
+++ routes/student.js
@@ -2,7 +2,6 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const studentController = require("../controllers/student");
-const calendarController = require("../controllers/calendar");
 
 router.get("/search_batches", studentController.getSearchBatches);
 router.get("/enroll_batch", studentController.enrollBatch);
@@ -12,7 +11,5 @@ router.get("/response", studentController.getResponse);
 router.get("/response_del", studentController.deleteResponse);
 router.get("/assignments", studentController.getAllAssignments);
 router.get("/batch_assignments", studentController.getBatchAssignments);
-
-router.post("/create", calendarController.createEvent);
 
 module.exports = router;
